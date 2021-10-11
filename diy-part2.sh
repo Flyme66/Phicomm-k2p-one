032f3df4fa0be608a3b75098bdd8350194393566
AK201314 <88940210+AK201314@users.noreply.github.com>
2021-10-12 02:26:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,9 +9,11 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+
+
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改密码为空，自定义名称
 ZZZ="package/default-settings/files/zzz-default-settings"
