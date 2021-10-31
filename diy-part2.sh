ffdf5a314af4744bf4c9dfba45f02cb9df6527ef
AK201314 <88940210+AK201314@users.noreply.github.com>
2021-10-31 19:59:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,15 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
+# 删除原来的argon
+rm -rf  package/lean/luci-theme-argon
 
+# 添加老竭力的argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+# 修改默认主题为argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
 
 # 修改密码为空，自定义名称
 ZZZ="package/default-settings/files/zzz-default-settings"
@@ -23,10 +30,10 @@ sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_
 
 sed -i '/set wireless.default_radio0.ssid=Phicomm-k2p_5G/a\ set wireless.default_radio1.ssid=Phicomm-k2p' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-#git clone https://github.com/small-5/luci-app-adblock-plus.git package/lean/luci-app-adblock-plus
+git clone https://github.com/small-5/luci-app-adblock-plus.git package/lean/luci-app-adblock-plus
 
-#git clone https://github.com/wiwizcom/WiFiPortal.git package/lean/wifidog-wiwiz
+git clone https://github.com/wiwizcom/WiFiPortal.git package/lean/wifidog-wiwiz
 
-#git clone https://github.com/fw876/helloworld package/lean/luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld package/lean/luci-app-ssr-plus
 
-#git clone https://github.com/xiaorouji/openwrt-passwall package/lean/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/lean/luci-app-passwall
