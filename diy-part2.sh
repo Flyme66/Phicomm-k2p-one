2401d65b192cecdf6ae8a2be337ad2c0d5fbe896
AK201314 <88940210+AK201314@users.noreply.github.com>
2022-05-26 23:49:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,8 +26,8 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改密码为空，自定义名称
-#ZZZ="package/lean/default-settings/files/zzz-default-settings"
-#sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
+ZZZ="package/emortal/default-settings/files/99-default-settings"
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 
 #sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio0.ssid=Phicomm-k2p_5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
